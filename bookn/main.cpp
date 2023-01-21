bcd5b2c11057ce3e6cd2e46f81f8e2463790dc58
vctrubio <vctrubio@gmail.com>
2023-01-21 21:59:37 +0100

    Does not compile, i will check another video and conquer you

--- bookn/main.cpp
+++ bookn/main.cpp
@@ -1,22 +1,40 @@
 #include "User.hpp"
 #include "Book.hpp"
-#include "./mysql/mysql.h"
+#include "mysql/mysql.h"
 
-void	create_user()
+struct connection_details
 {
-
-}
-
-void	all_user()
-{
-
-}
+		const char	*server, *user, *password, *database;
+};
 
 Book*	_DB()
 {
 	static Book	database;
 
 	return (&database);
+}
+
+MYSQL		*mysql_connection_setup(struct connection_details mysql_details)
+{
+		MYSQL	*connection;
+
+		connection = mysql_init(NULL);
+		if (!mysql_real_connect(connection, mysql_details.server, mysql_details.user, mysql_details.password, mysql_details.database, 0, NULL, 0))
+		{
+			cout << "Coneection ERROR" << mysql_error(connection) << endl;
+			exit(1);
+		}
+	return (connection);
+}
+
+MYSQL_RES	*mysql_execute_query(MYSQL *connection, const char *sql_query)
+{
+	if (mysql_query(connection, sql_query))
+	{
+		cout << "SQL query error\n" << mysql_error(connection) << endl;
+		exit(1);
+	}
+	return (mysql_use_result(connection));
 }
 
 int main()
@@ -33,6 +51,36 @@ int main()
 	database->lst_add(&u3);
 
 //
+
+	MYSQL						*con;
+	MYSQL_RES					*res;
+	MYSQL_ROW					row;
+	struct connection_details	mysqlD;
+
+	mysqlD.server = "localhost";
+	mysqlD.user = "trtp";
+	mysqlD.password = "password";
+	mysqlD.database = "test";
+
+	con = mysql_connection_setup(mysqlD);
+
+    // get the results from executing commands
+    res = mysql_execute_query(con, "select * from USER;");
+
+    std::cout << ("Database Output:\n") << std::endl;
+
+    while ((row = mysql_fetch_row(res)) != NULL){
+        // the below row[] parametes may change depending on the size of the table and your objective
+        std::cout << row[0] << " | " << row[1] << " | " << row[2] << " | " << row[3] << " | " << row[4] << std::endl << std::endl;
+    }
+
+    // clean up the database result
+    mysql_free_result(res);
+    
+    // close database connection
+    mysql_close(con);
+
+
 	int		input;
 	while (1)
 	{
@@ -41,7 +89,7 @@ int main()
 		if (input == 4)
 				break;
 		if (input == 1)
-				create_user();
+				;
 		if (input == 2)
 			database->printBook();
 	}
