66014579789652b3665b75048b60e878479ee48f
vctrubio <vctrubio@gmail.com>
2023-06-25 13:24:18 +0200

    try to build

--- my-react-app/my-inf/src/NavBar.js
+++ my-react-app/my-inf/src/NavBar.js
@@ -1,7 +1,4 @@
 import './App.css';
-import { useState } from 'react';
-import Page1 from './Page1';
-import Page2 from './Page2';
 
 export default function NavBar() {
     const scrollToPage = (pageId) => {
