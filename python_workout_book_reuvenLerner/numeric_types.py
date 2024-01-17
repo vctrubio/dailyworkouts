40ab827859562102886e2ee4d6fa57f68d7d0835
vctrubio <vctrubio@gmail.com>
2024-01-17 15:04:08 +0100

    v2 or ex01

--- python_workout_book_reuvenLerner/numeric_types.py
+++ python_workout_book_reuvenLerner/numeric_types.py
@@ -2,21 +2,32 @@ import sys
 import random
 
 def ex01():
-    number = random.randint(0, 30)
-    print(number)
-    
+    number = random.randint(0, 100)
+   
+    while True:
+        ui = int(input('input>'))
+        if ui == number:
+            break
+        if ui < number:
+            print('higher')
+        else:
+            print('lower')
+    print('ex01 complete; random_number = ', number)
+       
 
 
 
 if __name__ == '__main__':
     try:
+        if len(sys.argv) < 2:
+            raise IndexError("argv is less than 2")
         argv1 = sys.argv[1]
-        try:
-            ft_call = globals()[argv1]
-            ft_call()
-        except Exception:
-            print("no correct argument")
-    except Exception:
-        print("No argumentes given")
+        if argv1 in globals():
+            ft = globals()[argv1]
+            ft()
+        else:
+            raise NameError("function does not exist")
+    except Exception as e:
+        print(f'{e}')
     finally:
-        print('complete.')
+        print('Pcomplete.')
