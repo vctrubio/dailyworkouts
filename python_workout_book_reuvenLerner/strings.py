0d04c337558354368bf458fe78e2f07c90cb0d08
vctrubio <vctrubio@gmail.com>
2024-01-20 12:03:46 +0100

    complete string series

--- python_workout_book_reuvenLerner/strings.py
+++ python_workout_book_reuvenLerner/strings.py
@@ -2,8 +2,7 @@
 import sys
 
 #pig latin
-def ex05():
-    ui = input("input str: ")
+def ex05(ui):
     mod = ui[1:]
     if ui[0] in 'aeiou':
         mod += 'way'
@@ -11,8 +10,41 @@ def ex05():
         let = ui[0:1]
         mod += let
         mod += 'ay'
-    print(mo[::[::2]])
+    print(mod)
 
+#pig latin sentence
+def ex06():
+    ui = input("input str: ")
+    ui = ui.split()
+    for word in ui:
+       ex05(word)
+
+#prefix ub 
+def ex07():
+    ui = input("input str: ")
+    output = []
+    vowel = 'aeiou'
+    ui = ui.split()
+    for it, word in enumerate(ui):
+        for i in word:
+            if i in vowel:
+                output.append('ub')
+            output.append(i)
+        if it != len(ui) - 1:     
+            output.append(' ')
+    
+    output = ''.join(output)
+    print(output)
+
+#sorting a string
+def ex08():
+    ui = input("input str: ")
+    sui = ui.replace(' ','')
+    sui = set(sui)
+    sui = sorted(sui)
+    bui = sorted(set(ui.replace(' ','')))
+    #print(sui) 
+    print(bui) 
 
 
 if __name__ == '__main__':
