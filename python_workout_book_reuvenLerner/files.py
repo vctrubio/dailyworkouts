7f61abed10d668ad31bc9b6279c2ef60b77f276b
vctrubio <vctrubio@gmail.com>
2024-02-01 21:20:42 +0100

    yesterday

--- python_workout_book_reuvenLerner/files.py
+++ python_workout_book_reuvenLerner/files.py
@@ -45,6 +45,44 @@ def ex21():
         mydic[file] = longest_word(file)
     return mydic
 
+import csv
+def ex22():
+    file1 = 'helloworld'
+    file2 = 'outerworl'
+    with open(file1) as passwrd:
+        with open(file2) as output:
+              infile = csv.reader(passwrd, delimeter=':')
+              outfile = csv.reader(output, delimeter='\t')
+              for line in infile:
+                    if len(line) > 1:
+                        outfile.write((line[0], line[2])    )                    
+
+import glob
+import json
+#json load vice verca
+def ex23():
+    scores = {}
+    for filename in glob.glob(f'scores/*.json'):
+        scores[filename] = {}
+        with open(filename) as f:
+            for result in json.load(f):
+                for x, y in result.items():
+                    scores[filename].setdefault(x, [])
+                    scores[filename][x].append(y)
+    
+    for ptr in scores:
+        print(ptr)
+        for subject, score in scores[ptr].items():
+            min_score = min(score)
+            max_score = max(score)
+            average_score = (sum(score) /
+                             len(score))
+            print(subject)
+            print(f'\tmin {min_score}')
+            print(f'\tmax {max_score}')
+            print(f'\taverage {average_score}')
+   
+
 if __name__ == '__main__':
     try:
         if len(sys.argv) < 2:
